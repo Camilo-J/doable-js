e83f26621cf038450c0f07bbd8beb1aac8178c99
Camilo-J <josecamilo1902@outlook.com>
2022-11-25 15:35:58 -0500

    Add method to identify the task by currentpage

--- src/store.js
+++ src/store.js
@@ -1,4 +1,38 @@
 import { fromLocalStorage, saveToLocalStorage } from "./utils.js";
+import { getTasks } from "./services/task-services.js";
+
+async function listTasks(option = null) {
+  let currentpage = option || STORE.currentPage;
+  let tasks = await getTasks();
+  let newT;
+
+  console.log(currentpage);
+  switch (currentpage) {
+    case "important":
+      newT = tasks.filter((task) => task.important === true);
+      STORE.setTasks(newT);
+      break;
+
+    case "Ncompleted":
+      newT = tasks.filter((task) => task.completed === false);
+      STORE.setTasks(newT);
+      break;
+
+    case "Important&Ncompleted":
+      newT = tasks.filter((task) => task.completed === false);
+      let newT2 = newT.filter((task) => task.important === true);
+      STORE.setTasks(newT2);
+      break;
+
+    case "homepage":
+      STORE.setTasks(tasks);
+      break;
+
+    default:
+      console.log("hola2");
+      break;
+  }
+}
 
 const STORE = {
   currentPage: fromLocalStorage("current-page") || "login",
@@ -13,6 +47,7 @@ const STORE = {
   },
   setTasks(tasks) {
     this.tasks = tasks;
+    saveToLocalStorage("Tasks", tasks);
   },
   addTask(task) {
     this.tasks.push(task);
@@ -20,6 +55,7 @@ const STORE = {
   deleteTask(id) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   },
+  listTasks,
 };
 
 export default STORE;
