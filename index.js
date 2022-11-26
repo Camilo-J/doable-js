be2bb2a8b51bc614cade8630e7d7222583f5390d
Camilo-J <josecamilo1902@outlook.com>
2022-11-25 19:48:05 -0500

    Add method to init app

--- index.js
+++ index.js
@@ -3,23 +3,18 @@ import LoginPage from "./src/pages/login-page.js";
 import STORE from "./src/store.js";
 import Homepage from "./src/pages/homepage.js";
 import { tokenKey, appKey } from "./src/config.js";
-import { getTasks } from "./src/services/task-services.js";
-// import { login } from "./src/services/session-services.js"
-// import { getUser } from "./src /services/user-services";
 const root = document.querySelector("#root");
 let modu;
 async function init() {
   try {
     const token = sessionStorage.getItem(tokenKey);
-    console.log(token);
+
     if (!token) throw new Error();
     // await STORE.fetchContacts();
 
-    // console.log(STORE.contacts);
     modu = Homepage;
 
-    let tasks = await getTasks();
-    STORE.setTasks(tasks);
+    await STORE.listTasks();
   } catch (error) {
     sessionStorage.removeItem(tokenKey);
     modu = LoginPage;
